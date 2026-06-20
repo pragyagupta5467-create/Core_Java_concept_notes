8f6701038a0799b6d94fa3f7634e2a47b1deb320
_Pragya__Gupta_ <pragyagupta5467@gmail.com>
2026-06-20 18:39:31 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,9 @@
+// A constant value which can be assigned to variable called literal;
+public class literals {
+    public static void main(String[]args){
+       byte age=34;
+       char ch='A';
+       float d=5.6f;
+       System.out.println(age);
+    }
+}
